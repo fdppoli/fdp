dbf802734560e35b1c954b0666f27d54a282f18c
julietasegovia <julietavsegovia@gmail.com>
2026-05-21 21:06:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ L.tileLayer('https://{s}.tile.opennstreetmap.org/{z}/{x}/{y}.png', {
 
 L.marker([lat, lng])
 .addTo(map)
-.bindPopup('<b>Pecado Club</b><br>Laprida 847, Rosario')
+.bindPopup('<b>Viper</b><br>Laprida 847, Rosario')
 .openPopup();
